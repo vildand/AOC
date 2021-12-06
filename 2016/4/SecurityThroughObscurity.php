d976c05bfe40513bc2fde8e6b822ed41799d5f22
vildand <martin.wielandt@cellpointmobile.com>
2021-12-07 00:45:50 +0100

    day 4 star 2

--- 2016/4/SecurityThroughObscurity.php
+++ 2016/4/SecurityThroughObscurity.php
@@ -10,7 +10,7 @@ class SecurityThroughObscurity
   {
     $this->input = file_get_contents($filename);
     echo $this->getAnswer1();
-    //echo $this->getAnswer2();
+    echo $this->getAnswer2();
   }
 
   public function getAnswer1():string
@@ -25,7 +25,7 @@ class SecurityThroughObscurity
       //pop and isolate control values
       $matches = [];
       preg_match_all('#(\d+)\[([a-z]+)\]#', array_pop($aStack), $matches);
-      $sControlNum = $matches[1][0];
+      $iControlNum = intval($matches[1][0]);
       $sControlStr = $matches[2][0];
 
       //atomize and sort
@@ -45,39 +45,65 @@ class SecurityThroughObscurity
         $aaRes[strlen($match)][$match[0]] = $match[0];
       }
 
-      //match and final sort
+      //match and filter
       $bRealRoom = true;
       foreach ($aaRes as $aRes) {
         while($aRes) {
           if (array_key_exists($sControlStr[0], $aRes)){
             unset ($aRes[$sControlStr[0]]);
             $sControlStr = substr($sControlStr, 1);
-            if (!$sControlStr) {
-              break;
-            }
+            if (!$sControlStr) break;
           } else {
             $bRealRoom = false;
             break;
           }
         }
-        if (!$bRealRoom || !$sControlStr){
-          break;
-        }
+        if (!$bRealRoom || !$sControlStr) break;
       }
-      if ($bRealRoom) {
-        $iSum += intval($sControlNum);
-      }
+      $iSum += $bRealRoom ? $iControlNum:0;
     }
-
     return "Answer1 = ".$iSum."\n";
-
   }
 
   public function getAnswer2()
   {
+    $seq = 'abcdefghijklmnopqrstuvwxyz';
+    $len = strlen($seq);
     $aLines = explode("\n", $this->input);
+
     foreach ($aLines as $line) {
+      //chunk
+      $aStack = explode( "-", $line);
+
+      //pop and isolate control value
+      $matches = [];
+      preg_match_all('#(\d+)\[([a-z]+)\]#', array_pop($aStack), $matches);
+      $iControlNum = intval($matches[1][0]);
+      $iOffset = $iControlNum%$len;
+
+      //atomize string
+      $stringParts = str_split(implode(' ', $aStack));
+      $aRes = [];
+      //foreach rotate
+      for($c=0;$c<count($stringParts);$c++) {
+        if ($stringParts[$c] === ' ') {
+          $aRes[] = ' ';
+          continue;
+        }
+        $seqPos = strpos($seq, $stringParts[$c]);
+        $newPos = ($seqPos+$iOffset >= $len) ? ($iOffset-($len-$seqPos)) : $seqPos+$iOffset;
+        $aRes[$c]=substr($seq, $newPos, 1);
+      }
+
+      //concat string
+      $sRes=implode($aRes);
+
+      //break and echo result if found
+      if (preg_match_all('#northpole#',$sRes, $matches)) break;
+
     }
-    return "Answer2 = " . "\n";
+
+    return "Answer2 = " . $iControlNum . "\n";
+
   }
 }
